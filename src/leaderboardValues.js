64c415470152124fa9cb3a8d237796321efbf043
Nicky Pochinkov <pochinkn@tcd.ie>
2020-05-19 22:15:33 +0100

    Added test leaderboard values

---
+++ src/leaderboardValues.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    pos: 1,
+    name: "John Johnson",
+    dist: 6012
+  },{
+    pos: 2,
+    name: "Sarah Sorison",
+    dist: 5880
+  },{
+    pos: 3,
+    name: "Hugh Jass",
+    dist: 5707
+  },{
+    pos: 4,
+    name: "Francis Ffrench",
+    dist: 5666
+  },{
+    pos: 5,
+    name: "First Lastname",
+    dist: 5651
+  },{
+    pos: 6,
+    name: "Lauren O'Reilly",
+    dist: 5501
+  },{
+    pos: 7,
+    name: "Henry Reynolds",
+    dist: 5321
+  },{
+    pos: 8,
+    name: "Fake Name",
+    dist: 5011
+  },{
+    pos: 9,
+    name: "Personne de Faux Nom",
+    dist: 4980
+  },{
+    pos: 10,
+    name: "Miggldey Higgins",
+    dist: 4870
+  }
+]
